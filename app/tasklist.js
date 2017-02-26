20893a3a737b534df52a771883db779744e09376
Curtis Frifeldt <cfrifel@hawaii.edu>
2017-02-26 08:27:44 -1000

    Task 2: Tasklist setup

---
+++ app/tasklist.js
@@ -0,0 +1,18 @@
+/**
+ * File: tasklist.js
+ * Name: Curtis Frifeldt
+ * Date: Feb 26, 2017
+ */
+Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isClient) {
+  Template.tasks.helpers({
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+}
+
+if (Meteor.isServer) {
+
+}
